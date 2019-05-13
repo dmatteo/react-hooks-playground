657717bde75051a8061f395408e4954ae0f62f0c
Domenico Matteo <domenico.matteo@coinbase.com>
2019-05-13 10:18:49 -0700

    Add edit functionalities to ToDoItem

--- src/ToDoItem.js
+++ src/ToDoItem.js
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
 
 export const ToDoItem = ({ idx, value, onRemove, onEdit}) => {
+    const [isEditable, setIsEditable] = useState(false);
+    const [draft, setDraft] = useState(value);
+
+    const onSaveDraft = () => {
+        onEdit(idx, draft);
+        setIsEditable(false);
+    };
+
     return (
         <Item>
-            <span>{value}</span>
+            {
+                isEditable
+                    ? (
+                        <Content>
+                            <Input
+                                type="text"
+                                value={draft}
+                                onChange={e => setDraft(e.target.value)}
+                            />
+                            <button onClick={onSaveDraft}>Save</button>
+                        </Content>
+                    )
+                    : <Content onClick={() => setIsEditable(true)}>{value}</Content>
+            }
             <RemoveItem onClick={() => onRemove(idx)}>X</RemoveItem>
         </Item>
     )
@@ -19,6 +40,16 @@ const Item = styled.article`
     box-shadow: grey 3px 3px 6px 0px;
 `;
 
+const Content = styled.div`
+    display: flex;
+    flex: 1;
+    justify-content: space-between;
+`;
+
 const RemoveItem = styled.button`
     font-size: 14px;
+`;
+
+const Input = styled.input`
+    flex: 1;
 `;
